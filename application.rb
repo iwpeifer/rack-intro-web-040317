13d7de82a92934d8ba85380f14716407352f8c46
iwpeifer <iwpeifer@gmail.com>
2017-04-24 10:04:27 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Isaac. And my name is Jamie!"
     resp.finish
   end
 
